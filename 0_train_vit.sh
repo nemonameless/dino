6e411d884ef3a700061bbb9a58da3df966edb100
nemonameless <nemonameless@qq.com>
2023-06-07 10:20:13 +0000

    fix and add git ignore

--- 0_train_vit.sh
+++ 0_train_vit.sh
@@ -4,5 +4,5 @@ python -m paddle.distributed.launch --gpus="0,1,2,3,4,5,6,7" main_dino.py \
     --patch_size 16 --epochs 100 \
     --data_path /paddle/dataset/ILSVRC2012 \
     --batch_size 64 \
-    ----num_workers 4 \
+    --num_workers 4 \
     --output_dir ./out
